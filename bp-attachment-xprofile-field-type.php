6b821e7b3b7f54c5feba098d83b4b1532fe75c6b
Ryan Williams <rwilliams@mla.org>
2017-09-22 15:40:33 -0400

    first

---
+++ bp-attachment-xprofile-field-type.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Bp Attachment Xprofile Field Type
+ * Plugin URI:      https://github.com/mlaa/bp-attachment-xprofile-field-type
+ * Description:     Custom XProfile field type for BuddyPress Attachments.
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     bp-attachment-xprofile-field-type
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bp_Attachment_Xprofile_Field_Type
+ */
